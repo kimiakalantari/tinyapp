61a9f76a8e48cbb0984b07a600d0f39ae8f8317f
kimiakalantari <kimiakalantari97@gmail.com>
2023-03-31 23:35:27 -0400

    Add a Second Route and Template

--- express_server.js
+++ express_server.js
@@ -5,8 +5,8 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {longURL: "http://www.lighthouselabs.ca"},
+  "9sm5xK": {longURL: "http://www.google.com"}
 };
 
 app.get("/", (req, res) => {
@@ -24,6 +24,11 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
